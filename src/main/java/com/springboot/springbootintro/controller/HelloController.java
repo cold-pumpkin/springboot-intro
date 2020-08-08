a0f624e05a1459e066996d7cd6f223ed00cd65a4
adorno <pch4623@gmail.com>
2020-08-09 02:03:27 +0900

    View 환경설정 완료

---
+++ src/main/java/com/springboot/springbootintro/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.springboot.springbootintro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("nickname", "콜드펌킨");
+        return "about-me";
+    }
+}
